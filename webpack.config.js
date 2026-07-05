955ba351f96f0661fdcab891fba0bffd7a482463
MutagenD <mutagen-d@protonmail.com>
2026-07-05 13:40:45 +0300

    build: add missing dependency

--- webpack.config.js
+++ webpack.config.js
@@ -27,11 +27,13 @@ const full = {
     fallback: {
       "stream": require.resolve("stream-browserify"),
       "buffer": require.resolve("buffer/"),
+      "process": require.resolve("process/browser"),
     }
   },
   plugins: [
     new webpack.ProvidePlugin({
       Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser',
     }),
   ],
 }
